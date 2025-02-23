c7d122e6abb6a93945e41e952a18cc6003a867ec
Marcos <devmarcos7@gmail.com>
2025-02-23 16:39:19 -0300

    definido canvas e scrollbar

--- main.py
+++ main.py
@@ -24,8 +24,13 @@ entrada_tarefa = tk.Entry(frame_container, font=('Garamond', 14), relief='flat',
 botao_add = tk.Button(frame_container, text='Adiconar Tarefa', bg='#4CAF50', fg='white', height=1, width=15, font=('Robot', 11), relief='flat').pack(side='left', padx=10)
 
 #definindo lista de tarefas
-
 frame_lista_tarefas = tk.Frame(win, bg='white').pack(fill='both', expand=True, padx=10, pady=10)
+
+#definindo canvas
+canvas = tk.Canvas(frame_lista_tarefas, bg="white").pack(side='left', fill='both', expand=True)
+
+#definindo scrollbar
+scrollbar = ttk.Scrollbar(frame_lista_tarefas, orient='vertical').pack(side='right', fill='y')
 
 
 #loop para funcionamento do programa
